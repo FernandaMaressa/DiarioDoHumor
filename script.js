d263e2cf2fabb25ee94702ac1471fb764d4dd390
FernandaMaressa <fernandamaressa04@gmail.com>
2025-06-07 09:43:45 -0300

    Função que destaca humor selecionado e exibe animação com imagem correspondente

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ function saveName() {
 }
 
 let humorSelecionado = '';
+
+function selecionarHumor(humor) {
+    humorSelecionado = humor;
+
+    const imagens = document.querySelectorAll('.humor-opcao img');
+    imagens.forEach(img => img.classList.remove('selecionado'));
+
+    const opcao = Array.from(document.querySelectorAll('.humor-opcao'))
+        .find(div => div.textContent.trim() === humor);
+
+    if (opcao) {
+        const img = opcao.querySelector('img');
+        if (img) {
+            img.classList.add('selecionado');
+        }
+    }
+
+    mostrarImagemEmocao(humor);
+}
